cbe3256288a724e9bf5dbbe261678a9e66c5ced9
AndreeaNenciuCrasi <aanicaa@yahoo.com>
2021-02-09 17:34:01 +0200

    Adds object validation in controller

--- src/main/java/com/example/projecttool/web/ProjectController.java
+++ src/main/java/com/example/projecttool/web/ProjectController.java
@@ -5,10 +5,14 @@ import com.example.projecttool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/project")
@@ -19,9 +23,34 @@ public class ProjectController {
 
 
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+
+        if(result.hasErrors()){
+            return new ResponseEntity<String>("Invalid Project Object", HttpStatus.BAD_REQUEST);
+        }
         Project project1 = projectService.saveOrUpdateProject(project);
-        return new ResponseEntity<Project>(project, HttpStatus.CREATED);
+        return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
     }
 
 }
+
+//@RequestBody
+//        Annotation indicating a method parameter should be bound to the body of the web request.
+//        The body of the request is passed through an HttpMessageConverter to resolve the method argument
+//        depending on the content type of the request. Optionally, automatic validation can be applied by annotating the argument with @Valid.
+
+
+//public interface BindingResult
+//        extends Errors
+//General interface that represents binding results. Extends the interface for error registration capabilities, allowing
+// for a Validator to be applied, and adds binding-specific analysis and model building.
+
+
+//@Valid
+//Another handy aspect of @Valid not mentioned above is that (ie: using Postman to test an endpoint) @Valid will format the output
+//        of an incorrect REST call into formatted JSON instead of a blob of barely readable text. This is very useful if you are
+//        creating a commercially consumable API for your users.
+
+
+//@RequestMapping.
+//        Simply put, the annotation is used to map web requests to Spring Controller methods.
