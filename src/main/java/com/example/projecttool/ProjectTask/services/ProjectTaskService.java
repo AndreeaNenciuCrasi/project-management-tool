5b63391728303a990611f45a112d05dbfd272906
AndreeaNenciuCrasi <aanicaa@yahoo.com>
2021-02-27 10:31:31 +0200

    Adds feature add project task

--- src/main/java/com/example/projecttool/ProjectTask/services/ProjectTaskService.java
+++ src/main/java/com/example/projecttool/ProjectTask/services/ProjectTaskService.java
@@ -1,6 +1,8 @@
 package com.example.projecttool.ProjectTask.services;
 
+import com.example.projecttool.Backlog.domain.Backlog;
 import com.example.projecttool.Backlog.repositories.BacklogRepository;
+import com.example.projecttool.ProjectTask.domain.ProjectTask;
 import com.example.projecttool.ProjectTask.repositories.ProjectTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,4 +15,23 @@ public class ProjectTaskService {
 
     @Autowired
     private ProjectTaskRepository projectTaskRepository;
+
+    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask){
+        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+        projectTask.setBacklog(backlog);
+        Integer backlogSequence = backlog.getPTSequence();
+        backlogSequence++;
+        backlog.setPTSequence(backlogSequence);
+
+        projectTask.setProjectSequence(projectIdentifier +"-"+backlogSequence);
+        projectTask.setProjectIdentifier(projectIdentifier);
+
+        if(projectTask.getPriority() == null){
+            projectTask.setPriority(3);
+        }
+        if(projectTask.getStatus()=="" || projectTask.getStatus() == null){
+            projectTask.setStatus("TO_DO");
+        }
+        return projectTaskRepository.save(projectTask);
+    }
 }
