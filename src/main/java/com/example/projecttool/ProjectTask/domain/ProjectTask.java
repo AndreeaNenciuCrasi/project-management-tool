92fe18ae5dfa84c560f39475c124e0eefd719ac7
AndreeaNenciuCrasi <aanicaa@yahoo.com>
2021-03-01 18:11:56 +0200

    Adds feat delete task

--- src/main/java/com/example/projecttool/ProjectTask/domain/ProjectTask.java
+++ src/main/java/com/example/projecttool/ProjectTask/domain/ProjectTask.java
@@ -24,7 +24,7 @@ public class ProjectTask {
     private Date dueDate;
 
     //Many to One with Backlog
-    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.REFRESH)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name="backlog_id", updatable = false, nullable = false)
     @JsonIgnore
     private Backlog backlog;
--- src/main/java/com/example/projecttool/ProjectTask/services/ProjectTaskService.java
+++ src/main/java/com/example/projecttool/ProjectTask/services/ProjectTaskService.java
@@ -75,5 +75,10 @@ public class ProjectTaskService {
         return projectTaskRepository.save(projectTask);
     }
 
+    public void deletePTByProjectSequence(String backlog_id, String pt_id){
+        ProjectTask projectTask = findPTByProjectSequence(backlog_id, pt_id);
+        projectTaskRepository.delete(projectTask);
+    }
+
 
 }
