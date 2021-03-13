6b95f2134cfa88ae4919acba119c14c8ab563d2f
AndreeaNenciuCrasi <aanicaa@yahoo.com>
2021-03-13 19:19:32 +0200

    Adds user into db

--- src/main/java/com/example/projecttool/ProjecttoolApplication.java
+++ src/main/java/com/example/projecttool/ProjecttoolApplication.java
@@ -3,9 +3,16 @@ package com.example.projecttool;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class ProjecttoolApplication {
+
+    @Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ProjecttoolApplication.class, args);
--- src/main/java/com/example/projecttool/User/domain/User.java
+++ src/main/java/com/example/projecttool/User/domain/User.java
@@ -1,14 +1,18 @@
 package com.example.projecttool.User.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.aspectj.bridge.IMessage;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import java.util.Collection;
 import java.util.Date;
 
 @Entity
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -96,5 +100,36 @@ public class User {
     @PreUpdate
     protected void onUpdate(){
         this.update_At = new Date();
+    }
+
+    //UserDetails interface methods
+    @Override
+    @JsonIgnore
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isEnabled() {
+        return true;
     }
 }
