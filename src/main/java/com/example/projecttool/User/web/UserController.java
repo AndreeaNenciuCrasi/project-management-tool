cdc85b128142c1c7563e4d7c1a5bdf28b932ffd8
AndreeaNenciuCrasi <aanicaa@yahoo.com>
2021-03-15 16:44:06 +0200

    Validates password

--- src/main/java/com/example/projecttool/User/web/UserController.java
+++ src/main/java/com/example/projecttool/User/web/UserController.java
@@ -4,6 +4,7 @@ package com.example.projecttool.User.web;
 import com.example.projecttool.Project.services.MapValidationErrorService;
 import com.example.projecttool.User.domain.User;
 import com.example.projecttool.User.services.UserService;
+import com.example.projecttool.validator.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,10 +26,13 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private UserValidator userValidator;
+
 
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result){
-
+        userValidator.validate(user, result);
         ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
         if(errorMap != null) return errorMap;
 
