3124d6537d3c1a51eb322145c72bd1881d548cdc
AndreeaNenciuCrasi <aanicaa@yahoo.com>
2021-08-09 16:33:53 +0300

    adds new team member to the project

--- src/main/java/com/example/projecttool/Team/controller/TeamMemberController.java
+++ src/main/java/com/example/projecttool/Team/controller/TeamMemberController.java
@@ -1,11 +1,34 @@
 package com.example.projecttool.Team.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.projecttool.Project.services.MapValidationErrorService;
+import com.example.projecttool.Status.model.Status;
+import com.example.projecttool.Team.model.TeamMember;
+import com.example.projecttool.Team.services.TeamMemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.security.Principal;
 
 @RestController
 @RequestMapping("/api/team")
 @CrossOrigin
 public class TeamMemberController {
+
+    @Autowired
+    private TeamMemberService teamMemberService;
+
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
+    @PostMapping("/{project_id}/{username}")
+    public ResponseEntity<?> addNewTeamMember(@RequestBody TeamMember teamMember,
+                                              @PathVariable String project_id, @PathVariable String username,  Principal principal){
+
+        TeamMember newTeamMember = teamMemberService.addTeamMemberToProject(teamMember,project_id, username);
+        return new ResponseEntity<TeamMember>(newTeamMember, HttpStatus.CREATED);
+    }
 }
--- src/main/java/com/example/projecttool/Team/services/TeamMemberService.java
+++ src/main/java/com/example/projecttool/Team/services/TeamMemberService.java
@@ -1,7 +1,31 @@
 package com.example.projecttool.Team.services;
 
+import com.example.projecttool.Project.repositories.ProjectRepository;
+import com.example.projecttool.Team.model.TeamMember;
+import com.example.projecttool.Team.repositories.TeamMemberRepository;
+import com.example.projecttool.User.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TeamMemberService {
+
+    private TeamMemberRepository teamMemberRepository;
+    private ProjectRepository projectRepository;
+    private UserRepository userRepository;
+
+    @Autowired
+    public TeamMemberService(TeamMemberRepository teamMemberRepository, ProjectRepository projectRepository, UserRepository userRepository) {
+        this.teamMemberRepository = teamMemberRepository;
+        this.projectRepository = projectRepository;
+        this.userRepository = userRepository;
+    }
+
+    public TeamMember addTeamMemberToProject(TeamMember teamMember, String project_id, String username){
+        Long userId = userRepository.findByUsername(username).getId();
+
+        teamMember.setProject(projectRepository.findByProjectIdentifier(project_id));
+        teamMember.setUserId(userId);
+        return teamMemberRepository.save(teamMember);
+    }
 }
