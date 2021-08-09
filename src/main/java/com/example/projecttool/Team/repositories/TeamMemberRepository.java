ab84cff9d235a707211ecd423b1bfd76aaa692ec
AndreeaNenciuCrasi <aanicaa@yahoo.com>
2021-08-09 15:38:43 +0300

    adds controller to team member

--- src/main/java/com/example/projecttool/Team/repositories/TeamMemberRepository.java
+++ src/main/java/com/example/projecttool/Team/repositories/TeamMemberRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TeamRepository extends CrudRepository<TeamMember,Long> {
+public interface TeamMemberRepository extends CrudRepository<TeamMember,Long> {
 }
--- src/main/java/com/example/projecttool/Team/services/TeamMemberService.java
+++ src/main/java/com/example/projecttool/Team/services/TeamMemberService.java
@@ -3,5 +3,5 @@ package com.example.projecttool.Team.services;
 import org.springframework.stereotype.Service;
 
 @Service
-public class TeamService {
+public class TeamMemberService {
 }
